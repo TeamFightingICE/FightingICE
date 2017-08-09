b529f677f7e435ad7b401ad7b308f5aeb9b0ad8c
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-08-09 16:15:10 +0900

    コメントを追記

--- src/manager/GameManager.java
+++ src/manager/GameManager.java
@@ -20,8 +20,10 @@ public abstract class GameManager {
 	/** BGMやSE等の音楽関連のタスクを管理するマネージャー */
 	protected SoundManager soundManager;
 
+	/** ゲームの終了要求があったかを表すフラグ */
 	private boolean isExitFlag;
 
+	/** フィールドの初期化を行うコンストラクタ */
 	public GameManager() {
 		this.graphicManager = new GraphicManager();
 		this.inputManager = new Input();
@@ -30,35 +32,49 @@ public abstract class GameManager {
 		this.isExitFlag = false;
 	}
 
-
 	public abstract void initialize();
+
 	public abstract void close();
 
+	/** 現在のゲームシーンの更新を行う */
 	public void update() {
-
 		inputManager.update();
 
 		if (!currentGameScene.isGameEnd()) {
 			if (currentGameScene.isTransition()) {
+
 				// 現在のシーンの終了処理
 				currentGameScene.close(graphicManager, soundManager, inputManager);
+
+				//遷移先のシーンを現在のシーンにセットし,初期化処理を行う
 				currentGameScene = currentGameScene.getNextGameScene();
 				currentGameScene.initialize(graphicManager, soundManager, inputManager);
 			}
 
+			//現在のシーンの更新
 			currentGameScene.update(graphicManager, soundManager, inputManager);
 		} else {
 			this.isExitFlag = true;
 		}
 	}
 
-	public void startGame(GameScene startGameScene){
+	/**
+	 * ゲームをスタートする<br>
+	 * 引数に指定したゲームのシーンからゲームが始まる
+	 *
+	 * @param startGameScene
+	 *            始めのゲームシーン
+	 */
+	public void startGame(GameScene startGameScene) {
 		this.currentGameScene = startGameScene;
 		this.currentGameScene.initialize(graphicManager, soundManager, inputManager);
 	}
 
-
-
+	/**
+	 * ゲームの終了要求があったかどうかを返す
+	 *
+	 * @return ゲームの終了要求があったかどうか
+	 */
 	public boolean isExit() {
 		return this.isExitFlag;
 	}
