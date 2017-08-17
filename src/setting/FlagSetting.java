ae312f32a663deffdae982c98195e8fba266a079
Ishii <is0245ve@ed.ritsumei.ac.jp>
2017-08-17 18:21:28 +0900

    前flagデータを一部コピー

--- src/setting/FlagSetting.java
+++ src/setting/FlagSetting.java
@@ -1,5 +1,15 @@
 package setting;
 
 public class FlagSetting {
+	/**
+	 * Use this value to output the number of each action player executed.
+	 */
+	public static boolean debugActionFlag = false;
+
+	public static boolean debugFrameDataFlag = false;
+
+	public static boolean trainingModeFlag = false;
+
+	public static boolean KinectSideFlag = false;
 
 }
