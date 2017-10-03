b5d95e414f52faa20fc9dfc8fb49fb374d8e5d0f
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-10-03 14:05:39 +0900

    Imageクラス構築

--- src/loader/ResourceLoader.java
+++ src/loader/ResourceLoader.java
@@ -1,5 +1,26 @@
 package loader;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class ResourceLoader {
 
+	public void loadImage(String filePath) {
+		BufferedImage bimg = null;
+
+		try {
+			bimg = ImageIO.read(new FileInputStream(new File(filePath)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void loadTextureFromImage(BufferedImage image){
+
+	}
 }
