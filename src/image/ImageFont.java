14f93140903c9b634bbe2afcdcd31f65d29bbec8
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-10-05 14:26:46 +0900

    StringTaskの構築

--- src/image/ImageFont.java
+++ src/image/ImageFont.java
@@ -1,6 +1,13 @@
 package image;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+import loader.ResourceLoader;
 
 public class ImageFont {
 
@@ -8,10 +15,88 @@ public class ImageFont {
 
 	private boolean antiAliasing;
 
+	private Image[] fontImage;
+
 
 	public ImageFont(Font font, boolean antiAliasing){
 		this.font = font;
 		this.antiAliasing = antiAliasing;
+		this.fontImage = new Image[256];
+
+		createFont();
+	}
+
+	public Image getCharacter(char whatChar){
+		return fontImage[whatChar];
+	}
+
+	/**
+	 * Create a standard Java2D BufferedImage of the given character
+	 *
+	 * @param ch
+	 *            The character to create a BufferedImage for
+	 *
+	 * @return A BufferedImage containing the character
+	 */
+	private BufferedImage getFontImage(char ch) {
+		// Create a temporary image to extract the character's size
+		BufferedImage tempFontImage = new BufferedImage(1, 1,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = (Graphics2D) tempFontImage.createGraphics();
+
+		if (antiAliasing) {
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+		}
+		g.setFont(font);
+		FontMetrics tempFontMetrics = g.getFontMetrics();
+
+		int charwidth = tempFontMetrics.charWidth(ch);
+		int charheight = tempFontMetrics.getHeight();
+		if (charwidth <= 0) {
+			charwidth = 1;
+		}
+		if (charheight <= 0) {
+			charheight = font.getSize();
+		}
+		tempFontImage = null;
+
+		// Create another image holding the character we are creating
+		BufferedImage charImage;
+		charImage = new BufferedImage(charwidth, charheight,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D gt = (Graphics2D) charImage.createGraphics();
+
+		if (antiAliasing == true) {
+			gt.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+		}
+		gt.setFont(font);
+		FontMetrics fontMetrics = g.getFontMetrics();
+		gt.setColor(Color.WHITE);
+
+		gt.drawString(String.valueOf(ch), 0, 0
+				+ fontMetrics.getAscent());
+
+		return charImage;
+
+	}
+
+	/**
+	 * Create and store the font
+	 *
+	 * @param customCharsArray Characters that should be also added to the cache.
+	 */
+	private void createFont() {
+
+		for (int i = 0; i < 256 ; i++) {
+			// get 0-255 characters and then custom characters
+			BufferedImage fontImage = getFontImage((char) i);
+
+			this.fontImage[i] = new Image();
+			this.fontImage[i] = ResourceLoader.getInstance().loadTextureFromBufferedImage(fontImage);
+		}
+
 	}
 
 }
--- src/loader/ResourceLoader.java
+++ src/loader/ResourceLoader.java
@@ -13,62 +13,76 @@ import javax.imageio.ImageIO;
 
 import image.Image;
 
+/** キャラクターの設定ファイルや画像等のリソースをロードするためのシングルトンなクラス */
 public class ResourceLoader {
 
-	/** 画像を読み込み，読み込んだ画像の情報を返す*/
+	private static ResourceLoader resourceLoader = new ResourceLoader();
+
+	private ResourceLoader(){
+		System.out.println("Create instance: " + ResourceLoader.class.getName());
+	}
+
+	public static ResourceLoader getInstance() {
+		return resourceLoader;
+	}
+
+	/** 画像を読み込み，読み込んだ画像の情報を返す */
 	public Image loadImage(String filePath) {
 		BufferedImage bimg = null;
 
 		try {
 			bimg = ImageIO.read(new FileInputStream(new File(filePath)));
+			return loadTextureFromBufferedImage(bimg);
 
-			// Gather all the pixels
-			int[] pixels = new int[bimg.getWidth() * bimg.getHeight()];
-			bimg.getRGB(0, 0, bimg.getWidth(), bimg.getHeight(), pixels, 0, bimg.getWidth());
-
-			// Create a ByteBuffer
-			ByteBuffer buffer = ByteBuffer.allocateDirect(bimg.getWidth() * bimg.getHeight() * 4)
-					.order(ByteOrder.nativeOrder());
-
-			// Iterate through all the pixels and add them to the ByteBuffer
-			for (int y = 0; y < bimg.getHeight(); y++) {
-				for (int x = 0; x < bimg.getWidth(); x++) {
-					// Select the pixel
-					int pixel = pixels[y * bimg.getWidth() + x];
-
-					// Add the RED component
-					buffer.put(((byte) ((pixel >> 16) & 0xFF)));
-					// Add the GREEN component
-					buffer.put(((byte) ((pixel >> 8) & 0xFF)));
-					// Add the BLUE component
-					buffer.put((byte) (pixel & 0xFF));
-					// Add the ALPHA component
-					buffer.put(((byte) ((pixel >> 24) & 0xFF)));
-
-				}
-			}
-
-			buffer.flip();
-
-			// Generate a texture ID
-			int textureId = glGenTextures();
-			// Bind the ID to the context
-			glBindTexture(GL_TEXTURE_2D, textureId);
-
-			// Setup texture scaling filtering
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-			// Send texture data to OpenGL
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, bimg.getWidth(), bimg.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
-					buffer);
-			buffer = null;
-
-			return new Image(textureId, bimg);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	public Image loadTextureFromBufferedImage(BufferedImage bimg){
+		// Gather all the pixels
+		int[] pixels = new int[bimg.getWidth() * bimg.getHeight()];
+		bimg.getRGB(0, 0, bimg.getWidth(), bimg.getHeight(), pixels, 0, bimg.getWidth());
+
+		// Create a ByteBuffer
+		ByteBuffer buffer = ByteBuffer.allocateDirect(bimg.getWidth() * bimg.getHeight() * 4)
+				.order(ByteOrder.nativeOrder());
+
+		// Iterate through all the pixels and add them to the ByteBuffer
+		for (int y = 0; y < bimg.getHeight(); y++) {
+			for (int x = 0; x < bimg.getWidth(); x++) {
+				// Select the pixel
+				int pixel = pixels[y * bimg.getWidth() + x];
+
+				// Add the RED component
+				buffer.put(((byte) ((pixel >> 16) & 0xFF)));
+				// Add the GREEN component
+				buffer.put(((byte) ((pixel >> 8) & 0xFF)));
+				// Add the BLUE component
+				buffer.put((byte) (pixel & 0xFF));
+				// Add the ALPHA component
+				buffer.put(((byte) ((pixel >> 24) & 0xFF)));
+
+			}
+		}
+		buffer.flip();
+
+		// Generate a texture ID
+		int textureId = glGenTextures();
+		// Bind the ID to the context
+		glBindTexture(GL_TEXTURE_2D, textureId);
+
+		// Setup texture scaling filtering
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		// Send texture data to OpenGL
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, bimg.getWidth(), bimg.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
+				buffer);
+		buffer = null;
+
+		return new Image(textureId, bimg);
 	}
 
 }
