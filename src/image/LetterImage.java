c7a414bd99caeb736a6b8f1c3eda0551c8a6bdcf
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-10-14 09:27:24 +0900

    配列サイズ変更

--- src/image/LetterImage.java
+++ src/image/LetterImage.java
@@ -18,19 +18,19 @@ public class LetterImage {
 	private Image[] letterImage;
 
 	public LetterImage() {
-		
+
 	}
 
-	public LetterImage(Font font, boolean antiAliasing){
+	public LetterImage(Font font, boolean antiAliasing) {
 		this.font = font;
 		this.antiAliasing = antiAliasing;
-		this.letterImage = new Image[256];
+		this.letterImage = new Image[96];
 
 		createFont();
 	}
 
-	public Image getCharacter(char whatChar){
-		return letterImage[whatChar];
+	public Image getLetterImage(char letter) {
+		return letterImage[letter];
 	}
 
 	/**
@@ -41,15 +41,13 @@ public class LetterImage {
 	 *
 	 * @return A BufferedImage containing the character
 	 */
-	private BufferedImage getLetterImage(char letter) {
+	private BufferedImage getLetterBufferedImage(char letter) {
 		// Create a temporary image to extract the letter's size
-		BufferedImage tempLetterImage = new BufferedImage(256, 256,
-				BufferedImage.TYPE_INT_ARGB);
+		BufferedImage tempLetterImage = new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = (Graphics2D) tempLetterImage.createGraphics();
 
 		if (antiAliasing) {
-			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-					RenderingHints.VALUE_ANTIALIAS_ON);
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		}
 		g.setFont(font);
 		FontMetrics fontMetrics = g.getFontMetrics();
@@ -65,35 +63,31 @@ public class LetterImage {
 		tempLetterImage = null;
 
 		// Create another image holding the character we are creating
-		BufferedImage letterImage = new BufferedImage(letterWidth,  letterHeight,
-				BufferedImage.TYPE_INT_ARGB);
+		BufferedImage letterImage = new BufferedImage(letterWidth, letterHeight, BufferedImage.TYPE_INT_ARGB);
 		g = (Graphics2D) letterImage.createGraphics();
 
 		if (antiAliasing) {
-			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-					RenderingHints.VALUE_ANTIALIAS_ON);
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		}
 		g.setFont(font);
 		g.setColor(Color.WHITE);
 
-		g.drawString(String.valueOf(letter), 0, 0
-				+ fontMetrics.getAscent());
+		g.drawString(String.valueOf(letter), 0, 0 + fontMetrics.getAscent());
 
 		return letterImage;
 
 	}
 
 	/**
-	 * 英数字及び記号文字のイメージを作成し，OpenGLにそのテクスチャを転送する．
-	 * ASCIIコード表から必要な部分のみを抽出
+	 * 英数字及び記号文字のイメージを作成し，OpenGLにそのテクスチャを転送する． ASCIIコード表から必要な部分のみを抽出
 	 */
 	private void createFont() {
 
-		for (int i = 32; i <= 126 ; i++) {
+		for (int i = 32; i <= 126; i++) {
 			// get 32-126 characters and then custom letters
-			BufferedImage fontImage = getLetterImage((char) i);
+			BufferedImage bi = getLetterBufferedImage((char) i);
 
-			this.letterImage[i] = ResourceLoader.getInstance().loadTextureFromBufferedImage(fontImage);
+			this.letterImage[i] = ResourceLoader.getInstance().loadTextureFromBufferedImage(bi);
 		}
 
 	}
