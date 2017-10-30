1ecb277edebf29f985cec1cc75af7ae064f09baf
Ishii <is0245ve@ed.ritsumei.ac.jp>
2017-10-30 12:26:05 +0900

    ゲームの設定にかかわる変数の追加

--- src/setting/GameSetting.java
+++ src/setting/GameSetting.java
@@ -7,4 +7,15 @@ public class GameSetting {
 	public static final int FPS = 60;
 
 	private static final String[] CHARACTER = { "ZEN", "GARNET", "LUD" };
+
+	public static final int roundFrameNumber = 3600;
+	public static final int breakTimeFrameNumber = 15;
+
+	public final int GRAVITY = 1;
+	public final int FRICTION = 1;
+
+	public static final int INPUT_LIMIT = 30;
+	public static final int ROUND_TIME = 60000;
+
+	public static final int ROUND_MAX = 3;
 }
