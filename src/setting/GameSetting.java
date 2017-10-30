eee3854851a88c26e3d3e721e4cf8052335c84df
Ishii <is0245ve@ed.ritsumei.ac.jp>
2017-10-30 12:41:30 +0900

    一部変数をstaticに、一部を大文字に変更

--- src/setting/GameSetting.java
+++ src/setting/GameSetting.java
@@ -8,11 +8,11 @@ public class GameSetting {
 
 	private static final String[] CHARACTER = { "ZEN", "GARNET", "LUD" };
 
-	public static final int roundFrameNumber = 3600;
-	public static final int breakTimeFrameNumber = 15;
+	public static final int ROUNDFRAMENUMBER = 3600;
+	public static final int BREAKTIMEFRAMENUMBER = 70;
 
-	public final int GRAVITY = 1;
-	public final int FRICTION = 1;
+	public static final int GRAVITY = 1;
+	public static final int FRICTION = 1;
 
 	public static final int INPUT_LIMIT = 30;
 	public static final int ROUND_TIME = 60000;
