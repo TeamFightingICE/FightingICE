97d77e29a30d74a01c9bc548fad872be5521010e
Ishii <is0245ve@ed.ritsumei.ac.jp>
2017-10-30 11:14:19 +0900

    character追加

--- src/setting/GameSetting.java
+++ src/setting/GameSetting.java
@@ -5,4 +5,6 @@ public class GameSetting {
 	public static final int STAGE_WIDTH = 960;
 	public static final int STAGE_HEIGHT = 640;
 	public static final int FPS = 60;
+
+	private static final String[] CHARACTER = { "ZEN", "GARNET", "LUD" };
 }
