be134f3240273a0e27b599cad6a4c32fb2132b51
Ishii <is0245ve@ed.ritsumei.ac.jp>
2017-11-02 14:18:29 +0900

    HitAreaの編集

--- src/struct/HitArea.java
+++ src/struct/HitArea.java
@@ -1,5 +1,24 @@
 package struct;
 
 public class HitArea {
+	/**
+	 * Value of arena`s left boundary
+	 */
+	private int L;
+
+	/**
+	 * Value of arena`s right boundary
+	 */
+	private int R;
+
+	/**
+	 * Value of arena`s top boundary
+	 */
+	private int T;
+
+	/**
+	 * Value of arena`s bottom boundary
+	 */
+	private int B;
 
 }
