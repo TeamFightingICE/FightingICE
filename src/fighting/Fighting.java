bc499bfdca78f33a8508a51b8cc8f30ee0404ee9
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-11-08 17:06:38 +0900

    結果格納用のクラスを作成

--- src/fighting/Fighting.java
+++ src/fighting/Fighting.java
@@ -1,5 +1,31 @@
 package fighting;
 
 public class Fighting {
+	
+	private Character[] playerCharacters;
+	
+	private int currentRound;
+	
+	
+
+	public Fighting(){
+		
+		this.playerCharacters = new Character[2];
+
+	}
+
+	public void initialize(){
+		
+		
+		
+		/////旧Fighting処理内容/////
+		
+		//BGMのロード
+		//SEロード
+		//画像系ロード
+		//スクリーン画像取得
+		//背景画像ロード
+
+	}
 
 }
--- src/gamescene/Play.java
+++ src/gamescene/Play.java
@@ -1,9 +1,25 @@
 package gamescene;
 
+import java.util.LinkedList;
+
+import fighting.Fighting;
+import struct.FrameData;
+
 public class Play extends GameScene {
+
+	private Fighting fighting;
+
+	private LinkedList<FrameData> framesDara;
+
+	private int nowFrame;
+
+	private int elapsedBreakTime;
+
+
 
 	@Override
 	public void initialize() {
+		this.fighting = new Fighting();
 
 
 	}
