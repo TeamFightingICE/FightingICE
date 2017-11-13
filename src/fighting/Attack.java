bd8da24a2d9198290ed05ce7f7a3420f9530c3ef
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-11-13 18:24:49 +0900

    Attackの実装

--- src/fighting/Attack.java
+++ src/fighting/Attack.java
@@ -3,18 +3,21 @@ package fighting;
 import struct.HitArea;
 
 public class Attack {
-	private HitArea hitAreaNow;
 
-	private int nowFrame;
+	private HitArea settingHitArea;
+
+	private int settingSpeedX;
+
+	private int settingSpeedY;
+
+	private HitArea currentHitArea;
+
+	private int currentFrame;
 
 	private boolean playerNumber;
 
-	private HitArea hitAreaSetting;
-
-	private int settingSpeedX;
-	private int settingSpeedY;
-
 	private int speedX;
+
 	private int speedY;
 
 	private int startUp;
@@ -22,14 +25,19 @@ public class Attack {
 	private int active;
 
 	private int hitDamage;
+
 	private int guardDamage;
 
 	private int startAddEnergy;
+
 	private int hitAddEnergy;
+
 	private int guardAddEnergy;
+
 	private int giveEnergy;
 
 	private int impactX;
+
 	private int impactY;
 
 	private int giveGuardRecov;
@@ -37,6 +45,126 @@ public class Attack {
 	private int attackType;
 
 	private boolean downProperty;
+
+	public Attack() {
+		this.settingHitArea = new HitArea();
+		this.settingSpeedX = 0;
+		this.settingSpeedY = 0;
+
+		this.currentHitArea = new HitArea();
+		this.currentFrame = -1;
+		this.playerNumber = true;
+		this.speedX = 0;
+		this.speedY = 0;
+		this.startUp = 0;
+		this.active = 0;
+
+		this.hitDamage = 0;
+		this.guardDamage = 0;
+		this.startAddEnergy = 0;
+		this.hitAddEnergy = 0;
+		this.guardAddEnergy = 0;
+		this.giveEnergy = 0;
+
+		this.impactX = 0;
+		this.impactY = 0;
+		this.giveGuardRecov = 0;
+		this.attackType = 0;
+		this.downProperty = false;
+	}
+
+	public Attack(Attack attack) {
+		this.settingHitArea = attack.getSettingHitArea();
+		this.settingSpeedX = attack.getSettingSpeedX();
+		this.settingSpeedY = attack.getSettingSpeedY();
+
+		this.currentHitArea = attack.getCurrentHitArea();
+		this.currentFrame = attack.getCurrentFrame();
+		this.playerNumber = attack.isPlayerNumber();
+		this.speedX = attack.getSpeedX();
+		this.speedY = attack.getSpeedY();
+		;
+		this.startUp = attack.getStartUp();
+		this.active = attack.getActive();
+
+		this.hitDamage = attack.getHitDamage();
+		this.guardDamage = attack.getGuardDamage();
+		this.startAddEnergy = attack.getStartAddEnergy();
+		this.hitAddEnergy = attack.getHitAddEnergy();
+		this.guardAddEnergy = attack.getGuardAddEnergy();
+		this.giveEnergy = attack.getGiveEnergy();
+
+		this.impactX = attack.getImpactX();
+		this.impactY = attack.getImpactY();
+		this.giveGuardRecov = attack.getGiveGuardRecov();
+		this.attackType = attack.getAttackType();
+		this.downProperty = attack.isDownProperty();
+	}
+
+	/**
+	 * 攻撃オブジェクトの初期化を行う
+	 *
+	 * @param playerNumber
+	 *            攻撃をP1かP2
+	 */
+	public void initialize(boolean playerNumber, int x, int y, int size, boolean direction) {
+		setParameters(x, y, size, direction);
+		this.playerNumber = playerNumber;
+		this.currentFrame = 0;
+	}
+
+	/**
+	 * 波動拳の当たり判定の座標位置と発生からの経過フレームを更新し, まだ攻撃がアクティブかどうかを返す
+	 *
+	 * @return 攻撃がまだアクティブかどうか
+	 */
+	public boolean updateProjectileAttack() {
+		this.currentHitArea.move(speedX, speedY);
+
+		return ++this.currentFrame <= this.active;
+	}
+
+	/**
+	 * 波動拳以外の攻撃の当たり判定の座標位置と発生からの経過フレームを更新し, まだ攻撃がアクティブかどうかを返す
+	 *
+	 * @param character
+	 *            攻撃を出したキャラクターの情報
+	 *
+	 * @return 攻撃がまだアクティブかどうか
+	 */
+	public boolean update(Character character) {
+		setParameters(character.getX(), character.getY(), character.getGraphicCenterX(), character.isFront());
+
+		return ++this.currentFrame <= this.active;
+	}
+
+	private void setParameters(int x, int y, int size, boolean direction) {
+		int left;
+		int right;
+		int top;
+		int bottom;
+
+		// when player faced right
+		if (direction) {
+			left = x + this.settingHitArea.getLeft();
+			right = x + this.settingHitArea.getRight();
+			this.speedX = this.settingSpeedX;
+			this.speedY = this.settingSpeedY;
+
+		} else {
+			left = x + size - this.settingHitArea.getRight();
+			right = x + size - this.settingHitArea.getLeft();
+			this.speedX = -this.settingSpeedX;
+			this.speedY = this.settingSpeedY;
+		}
+
+		top = y + this.settingHitArea.getTop();
+		bottom = y + this.settingHitArea.getBottom();
+
+		this.currentHitArea = new HitArea(left, right, top, bottom);
+	}
+
+	//////// Getter//////////
 
 	public boolean isPlayerNumber() {
 		return this.playerNumber;
@@ -47,8 +175,8 @@ public class Attack {
 	}
 
 	// get
-	public int getNowFrame() {
-		return this.nowFrame;
+	public int getCurrentFrame() {
+		return this.currentFrame;
 	}
 
 	public int getPlayerNumber() {
@@ -119,21 +247,29 @@ public class Attack {
 		return this.settingSpeedY;
 	}
 
-	// set
-	public void setNowFrame(int nowFrame) {
-		this.nowFrame = nowFrame;
+	public HitArea getSettingHitArea() {
+		return new HitArea(this.settingHitArea);
+	}
+
+	public HitArea getCurrentHitArea() {
+		return new HitArea(this.currentHitArea);
+	}
+
+	////// Setter//////
+	public void setCurrentFrame(int nowFrame) {
+		this.currentFrame = nowFrame;
 	}
 
 	public void setPlayerNumber(boolean playerNumber) {
 		this.playerNumber = playerNumber;
 	}
 
-	public void setSpeedX(int speed_x) {
-		this.speedX = speed_x;
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
 	}
 
-	public void setSpeedY(int speed_y) {
-		this.speedY = speed_y;
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
 	}
 
 	public void setStartUp(int startUp) {
