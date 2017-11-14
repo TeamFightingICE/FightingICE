1f99a058cbd6a3a14ddfa328a60169329635ae1c
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-11-14 13:56:44 +0900

    コンボテーブルとキャラクターの画像情報等を読み込む処理を追加

--- src/fighting/Character.java
+++ src/fighting/Character.java
@@ -1,11 +1,18 @@
 package fighting;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.javatuples.Triplet;
+
 import enumerate.Action;
 import enumerate.State;
+import loader.ResourceLoader;
 import struct.Key;
 
 public class Character {
@@ -54,6 +61,12 @@ public class Character {
 	private ArrayList<Action> currentCombo;
 
 	private int lastCombo;
+
+	/**
+	 * Combo table of this character.
+	 * May be NULL if the corresponding file is not found
+	 */
+	private ArrayList<Triplet<ArrayList<Action>, ArrayList<Action>, Integer>> comboTable;
 
 	private Deque<Key> inputCommands;
 
@@ -111,7 +124,28 @@ public class Character {
 		this.lastCombo = character.getLastCombo();
 	}
 
-	public void initialize(boolean playerNumber) {
+	public void initialize(String characterName, boolean playerNumber) {
+		try {
+			// gSetting.txtは名前や内容変える可能性大
+			// graphicの情報も入れるか要検討
+			BufferedReader br = ResourceLoader.getInstance()
+					.openReadFile("./data/character/" + characterName + "/gSetting.txt");
+			String[] size = br.readLine().split(",", 0);
+			String[] center = br.readLine().split(",", 0);
+
+			this.graphicSizeX = Integer.valueOf(size[0]);
+			this.graphicSizeY = Integer.valueOf(size[1]);
+			this.graphicCenterX = Integer.valueOf(center[0]);
+			this.graphicCenterY = Integer.valueOf(center[1]);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.comboTable = new ArrayList<Triplet<ArrayList<Action>, ArrayList<Action>, Integer>>();
+		this.inputCommands = new LinkedList<Key>();
+		this.processedCommands = new LinkedList<Key>();
+
+		setComboTable(characterName);
 
 	}
 
@@ -335,6 +369,111 @@ public class Character {
 	public void setGraphicSizeY(int graphicSizeY) {
 		this.graphicSizeY = graphicSizeY;
 	}
+
+	/**
+	 *
+	 * Sets the combo table.
+	 *
+	 * @param characterName the name of the Character.
+	 *
+	 */
+	private void setComboTable(String characterName) {
+		try{
+			BufferedReader br = ResourceLoader.getInstance().openReadFile("./data/character/"+characterName+"/ComboTable.csv");
+
+			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(br);
+			for (CSVRecord record : records) {
+				ArrayList<Action> actions = new ArrayList<Action>();
+				actions.add(Action.valueOf(record.get("1st attack")));
+				actions.add(Action.valueOf(record.get("2nd attack")));
+				actions.add(Action.valueOf(record.get("3rd attack")));
+				actions.add(Action.valueOf(record.get("4th attack")));
+				ArrayList<Action> breakers = new ArrayList<Action>();
+				for (String breaker : record.get("combo breaker").split(",")) {
+					breakers.add(Action.valueOf(breaker));
+				}
+				this.comboTable.add(Triplet.with(actions, breakers, Integer.parseInt(record.get("extra damage"))));
+			}
+
+			br.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 *
+	 * Sets motions.
+	 *
+	 * @param characterName the character's name.
+	 */
+	/*private void setMotionVector(String characterName) {
+		try{
+			File csv = new File("./data/character/"+characterName+"/Motion.csv");
+
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+
+			Vector<MotionData> motionData = new Vector<MotionData>();
+
+			String line;
+			br.readLine(); // ignore header
+
+			while((line = br.readLine()) != null)
+			{
+				String[] st = line.split(",",0);
+
+				MotionData tmp = new MotionData();
+
+				tmp.motionName = st[0];
+				tmp.frameNumber = Integer.valueOf(st[1]);
+				tmp.speedX = Integer.valueOf(st[2]);
+				tmp.speedY = Integer.valueOf(st[3]);
+				tmp.hit = new HitArea(Integer.valueOf(st[4]),Integer.valueOf(st[5]),Integer.valueOf(st[6]),Integer.valueOf(st[7]));
+				tmp.state= State.valueOf(st[8]);
+				tmp.attackHit = new HitArea(Integer.valueOf(st[9]),Integer.valueOf(st[10]),Integer.valueOf(st[11]),Integer.valueOf(st[12]));
+				tmp.attackSpeedX = Integer.valueOf(st[13]);
+				tmp.attackSpeedY = Integer.valueOf(st[14]);
+				tmp.attackStartUp = Integer.valueOf(st[15]);
+				tmp.attackInterval = Integer.valueOf(st[16]);
+				tmp.attackRepeat = Integer.valueOf(st[17]);
+				tmp.attackActive = Integer.valueOf(st[18]);
+				tmp.attackHitDamage = Integer.valueOf(st[19]);
+				tmp.attackGuardDamage = Integer.valueOf(st[20]);
+				tmp.attackStartAddEnergy = Integer.valueOf(st[21]);
+				tmp.attackHitAddEnergy = Integer.valueOf(st[22]);
+				tmp.attackGuardAddEnergy = Integer.valueOf(st[23]);
+				tmp.attackGiveEnergy = Integer.valueOf(st[24]);
+				tmp.attackImpactX = Integer.valueOf(st[25]);
+				tmp.attackImpactY = Integer.valueOf(st[26]);
+				tmp.attackGiveGuardRecov = Integer.valueOf(st[27]);
+				tmp.attackKnockBack = Integer.valueOf(st[28]);
+				tmp.attackHitStop = Integer.valueOf(st[29]);
+				tmp.attackType = Integer.valueOf(st[30]);
+				tmp.attackDownProperty =  Boolean.valueOf(st[31]);
+				tmp.attackImage = st[32];
+				tmp.cancelAbleFrame = Integer.valueOf(st[33]);
+				tmp.cancelAbleMotionLevel = Integer.valueOf(st[34]);
+				tmp.motionLevel = Integer.valueOf(st[35]);
+				tmp.control = Boolean.valueOf(st[36]);
+				tmp.landingFlag = Boolean.valueOf(st[37]);
+
+				motionData.addElement(tmp);
+			}
+
+			this.motionVector = new Vector<Motion>();
+
+			for(Action act : Action.values()){
+				Motion motionObject = new Motion();
+				motionObject.setParameters(motionData.elementAt(act.ordinal()),characterName,ic);
+				motionVector.addElement(motionObject);
+			}
+
+			br.close();
+
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}*/
 
 	/**
 	 * Sets a list storing keys of the action that the character will be
