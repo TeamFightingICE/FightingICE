f9e9a9069c94894a95d3fab01bb32740918c9497
itosgr <is0202iv@ed.ritsumei.ac.jp>
2017-12-04 06:51:19 +0900

    CharacterDataのコンストラクタ修正

--- src/struct/CharacterData.java
+++ src/struct/CharacterData.java
@@ -125,6 +125,8 @@ public class CharacterData {
 		this.hitConfirm = character.isHitConfirm();
 		this.hitCount = character.getHitCount();
 		this.lastHitFrame = character.getLastHitFrame();
+		this.inputCommands = character.getInputCommand();
+		this.processedCommands = character.getProcessedCommand();
 	}
 
 	// Copy constructor for the CharacterData class
@@ -152,6 +154,8 @@ public class CharacterData {
 		this.hitConfirm = characterData.isHitConfirm();
 		this.hitCount = characterData.getHitCount();
 		this.lastHitFrame = characterData.getLastHitFrame();
+		this.inputCommands = characterData.getInputCommand();
+		this.processedCommands = characterData.getProcessedCommand();
 	}
 
 	/**
