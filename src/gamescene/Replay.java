1e1ba6180cbcc8630e1f6e95c1193edbaa676937
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-12-05 18:22:31 +0900

    Replayにjavadocを追記

--- src/gamescene/Replay.java
+++ src/gamescene/Replay.java
@@ -27,30 +27,46 @@ import util.ResourceDrawer;
 
 public class Replay extends GameScene {
 
+	/** 対戦処理を行うクラスのインスタンス */
 	private Fighting fighting;
 
+	/** Replayファイルからログを読み込むための入力ストリーム */
 	private DataInputStream dis;
 
+	/** 現在のフレームナンバー */
 	private int nowFrame;
 
+	/** 各ラウンド前に行う初期化処理内における経過フレーム数 */
 	private int elapsedBreakTime;
 
+	/** 現在のラウンド */
 	private int currentRound;
 
+	/** 各ラウンドの開始時かどうかを表すフラグ */
 	private boolean roundStartFlag;
 
+	/** 対戦処理後のキャラクターデータといったゲーム情報を格納したフレームデータ */
 	private FrameData frameData;
 
+	/** 対戦処理後のゲーム画面の情報 */
 	private ScreenData screenData;
 
+	/** 対戦処理に用いるP1, P2の入力情報 */
 	private KeyData keyData;
 
+	/** 再生速度を指定するインデックス */
 	private int playSpeedIndex;
 
+	/** 再生速度を管理する配列 */
 	private int[] playSpeedArray;
 
+	/** ラウンドが終わったかどうかを表すフラグ */
 	private boolean isFinished;
 
+	/**
+	 * Replayシーンを初期化するコンストラクタ<br>
+	 * このコンストラクタ内で読み込むReplayファイルをopenする.
+	 */
 	public Replay() {
 		// 以下4行の処理はgamesceneパッケージ内クラスのコンストラクタには必ず含める
 		this.gameSceneName = GameSceneName.REPLAY;
@@ -103,7 +119,9 @@ public class Replay extends GameScene {
 				this.elapsedBreakTime++;
 
 			} else {
+				// 再生速度を更新する
 				updatePlaySpeed();
+
 				// processing
 				for (int i = 0; i < this.playSpeedArray[this.playSpeedIndex] && !this.isFinished; i++) {
 					processingGame();
@@ -155,6 +173,7 @@ public class Replay extends GameScene {
 
 	}
 
+	/** 各ラウンド開始時における, インターバル処理を行う */
 	private void processingBreakTime() {
 		// ダミーフレームをAIにセット
 		InputManager.getInstance().setFrameData(new FrameData(), new ScreenData());
@@ -163,6 +182,14 @@ public class Replay extends GameScene {
 		GraphicManager.getInstance().drawString("Waiting for Round Start", 350, 200);
 	}
 
+	/**
+	 * 対戦処理を行う.<br>
+	 *
+	 * 1. P1, P2の入力を受け取る.<br>
+	 * 2. 対戦処理を行う.<br>
+	 * 3. 対戦後のFrameData, 及びScreenDataを取得する.<br>
+	 * 4. ラウンドが終了しているか判定する.<br>
+	 */
 	private void processingGame() {
 		this.keyData = createKeyData();
 
@@ -176,21 +203,33 @@ public class Replay extends GameScene {
 		}
 	}
 
+	/** 各ラウンド終了時の処理を行う. */
 	private void processingRoundEnd() {
 		this.isFinished = true;
 		this.currentRound++;
 		this.roundStartFlag = true;
 	}
 
+	/**
+	 * キャラクターが倒されたかどうかを判定する.
+	 *
+	 * @return true: P1 or P2が倒された; false: otherwise
+	 */
 	private boolean isBeaten() {
 		return FlagSetting.limitHpFlag
 				&& (this.frameData.getCharacter(true).getHp() <= 0 || this.frameData.getCharacter(false).getHp() <= 0);
 	}
 
+	/**
+	 * 1ラウンドの制限時間が経過したかどうかを判定する.<br>
+	 *
+	 * @return true: 1ラウンドの制限時間が経過した; false: otherwise
+	 */
 	private boolean isTimeOver() {
 		return this.nowFrame == GameSetting.ROUND_FRAME_NUMBER - 1;
 	}
 
+	/** 各ラウンド開始時に, 対戦情報や現在のフレームなどの初期化を行う */
 	private void initRound() {
 		this.fighting.initRound();
 		this.nowFrame = 0;
@@ -199,6 +238,11 @@ public class Replay extends GameScene {
 		this.isFinished = false;
 	}
 
+	/**
+	 * 対戦処理に用いるP1, P2の入力キーを作成する.<br>
+	 *
+	 * @return P1, P2の入力キー
+	 */
 	private KeyData createKeyData() {
 		Key[] temp = new Key[2];
 
@@ -266,6 +310,7 @@ public class Replay extends GameScene {
 		return i == 1 ? true : false;
 	}
 
+	/** 使用キャラクターや最大HPといったヘッダ情報を読み込む */
 	private void readHeader() {
 		for (int i = 0; i < 2; i++) {
 			try {
@@ -288,6 +333,7 @@ public class Replay extends GameScene {
 		}
 	}
 
+	/** Replayの再生速度を更新する. */
 	private void updatePlaySpeed() {
 		Key key = InputManager.getInstance().getKeyData().getKeys()[0];
 
