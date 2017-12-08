3777f9de7c6dffe250d48a99ded0debe310e1054
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-12-08 17:57:45 +0900

    ゲートウェイに関するJavadocを追加

--- src/python/PyGatewayServer.java
+++ src/python/PyGatewayServer.java
@@ -4,14 +4,19 @@ import gamescene.Python;
 import py4j.GatewayServer;
 import setting.LaunchSetting;
 
+/** Python側でJava側の処理を行うためのゲートウェイの管理を行うクラス. */
 public class PyGatewayServer {
+
+	/** Python側でJava側の処理を行うためのゲートウェイ */
 	private GatewayServer gatewayServer;
 
+	/** 指定されたゲームシーンでゲートウェイを作成するコンストラクタ */
 	public PyGatewayServer(Python python) {
 		this.gatewayServer = new GatewayServer(new PyManager(python), LaunchSetting.py4jPort);
 		this.gatewayServer.start();
 	}
 
+	/** ゲートウェイを閉じる */
 	public void close() {
 		this.gatewayServer.shutdown();
 		LaunchSetting.pyGatewayServer = null;
