d293411666a0196813c6ff3e64150e8a391ed6e8
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-12-11 18:07:53 +0900

    エフェクトのJavadoc追加

--- src/fighting/Effect.java
+++ src/fighting/Effect.java
@@ -2,14 +2,38 @@ package fighting;
 
 import image.Image;
 
+/** 攻撃とそれに対応するエフェクトの画像を管理する親クラス */
 public class Effect {
+
+	/** 1枚の画像を表示させる上限のフレーム数 */
 	protected static final int FRAME_PER_IMAGE = 5;
 
+	/**
+	 * The attack related to this effect
+	 */
 	protected Attack attack;
+
+	/**
+	 * The all of effect's images
+	 */
 	protected Image[] hitImages;
+
+	/** エフェクトが生成されてからの経過時間 */
 	protected int currentFrame;
+
+	/** 1枚の画像を表示させるフレーム数 */
 	protected int framesPerImage;
 
+	/**
+	 * 指定されたデータでエフェクトのインスタンスを作成するコンストラクタ
+	 *
+	 * @param attack
+	 *            攻撃オブジェクト
+	 * @param image
+	 *            攻撃オブジェクトに対応する全てのエフェクト画像
+	 * @param framesPerImage
+	 *            1枚のエフェクト画像の表示フレーム数
+	 */
 	public Effect(Attack attack, Image[] hitImages, int framesPerImage) {
 		this.attack = attack;
 		this.hitImages = hitImages;
@@ -17,23 +41,29 @@ public class Effect {
 		this.framesPerImage = framesPerImage;
 	}
 
+	/**
+	 * 指定されたデータでエフェクトのインスタンスを作成するコンストラクタ
+	 *
+	 * @param attack
+	 *            攻撃オブジェクト
+	 * @param image
+	 *            攻撃オブジェクトに対応する全てのエフェクト画像
+	 */
 	public Effect(Attack attack, Image[] hitImages) {
 		this(attack, hitImages, FRAME_PER_IMAGE);
 	}
 
 	/**
-	 *
 	 * Updates the effect's state.
 	 *
-	 * @return a boolean which indicate the if the effects is finish or not.
+	 * @return true: effect display time has not yet elapsed; false: otherwise
 	 */
 	public boolean update() {
 		return ++this.currentFrame < (this.hitImages.length * this.framesPerImage);
 	}
 
 	/**
-	 *
-	 * Returns effect's image.
+	 * Returns the effect's image.
 	 *
 	 * @return The effect's image.
 	 */
@@ -42,28 +72,20 @@ public class Effect {
 	}
 
 	/**
+	 * Returns the all of effect's images.
 	 *
-	 * Returns effect's image.
-	 *
-	 * @return The effect's image.
+	 * @return The all of effect's images.
 	 */
 	public Image[] getImages() {
 		return this.hitImages;
 	}
 
 	/**
-	 *
-	 * Returns attack related to this effect.
+	 * Returns the attack related to this effect.
 	 *
 	 * @return The attack related to this effect.
 	 */
 	public Attack getAttack() {
 		return this.attack;
 	}
-
-	public void setAttack(Attack attack) {
-		this.attack = attack;
-
-	}
-
 }
