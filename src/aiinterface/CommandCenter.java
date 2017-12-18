c48292764d0b18d3d7ab26b36ff5d31b5425c7f8
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2017-12-18 18:48:21 +0900

    スレッド・コマンド関連のJavadoc追加

--- src/aiinterface/CommandCenter.java
+++ src/aiinterface/CommandCenter.java
@@ -7,159 +7,178 @@ import struct.FrameData;
 import struct.Key;
 
 /**
- * AIから受け取ったアクション(名前,2 4 1 _ BとかL LD D _ Bとか)をKeyに変換して一連のキーを保存しておく
+ * AIから受け取ったアクションをKeyに変換し，変換後のキーを管理するクラス．
  */
 public class CommandCenter {
 
-	//private boolean skillFlag;
-
-	//実行待ちコマンドリスト
+	/**
+	 * 変換されたキーを格納するリスト．
+	 */
 	private LinkedList<Key> skillKey;
-	
+
+	/**
+	 * 現在のフレームデータ．
+	 */
 	private FrameData frameData;
 
+	/**
+	 * The character's side flag.<br>
+	 * {@code true} if the character is P1, or {@code false} if P2.
+	 */
 	private boolean playerNumber;
-	
-	public CommandCenter(){
+
+	/**
+	 * フィールド変数を初期化するクラスコンストラクタ．
+	 */
+	public CommandCenter() {
 		this.skillKey = new LinkedList<Key>();
 		this.frameData = new FrameData();
 		this.playerNumber = true;
 	}
 
 	/**
-	 * AIからコマンドを受け取って、実行待ちコマンドリストに保持しておく
-	 * @param command
+	 * AIが入力した実行アクション名をKeyに変換し，リストに格納する．<br>
+	 * リストにまだ処理されてないキーがある場合は，無視する．
+	 *
+	 * @param str
+	 *            アクション名
 	 */
-	public void commandCall(String str){
-		//実行待ちのコマンドが存在しない場合
-		if(this.skillKey.isEmpty()){
+	public void commandCall(String str) {
+		// 実行待ちのキーが存在しない場合
+		if (this.skillKey.isEmpty()) {
 			actionToCommand(str);
 		}
 	}
 
-	private void actionToCommand(String str){
-		switch(str){
-		case	"FORWARD_WALK"	:
+	/**
+	 * アクション名をコマンド名に変換し，そのコマンド名に対応したキーを作成する．
+	 *
+	 * @param str
+	 *            アクション名
+	 */
+	private void actionToCommand(String str) {
+		switch (str) {
+		case "FORWARD_WALK":
 			createKeys("6");
 			break;
-		case	"DASH"	:
+		case "DASH":
 			createKeys("6 5 6");
 			break;
-		case	"BACK_STEP"	:
+		case "BACK_STEP":
 			createKeys("4 5 4");
 			break;
-		case	"CROUCH"	:
+		case "CROUCH":
 			createKeys("2");
 			break;
-		case	"JUMP"	:
+		case "JUMP":
 			createKeys("8");
 			break;
-		case	"FOR_JUMP"	:
+		case "FOR_JUMP":
 			createKeys("9");
 			break;
-		case	"BACK_JUMP"	:
+		case "BACK_JUMP":
 			createKeys("7");
 			break;
-		case	"STAND_GUARD"	:
+		case "STAND_GUARD":
 			createKeys("4");
 			break;
-		case	"CROUCH_GUARD"	:
+		case "CROUCH_GUARD":
 			createKeys("1");
 			break;
-		case	"AIR_GUARD"	:
+		case "AIR_GUARD":
 			createKeys("7");
 			break;
-		case	"THROW_A"	:
+		case "THROW_A":
 			createKeys("4 _ A");
 			break;
-		case	"THROW_B"	:
+		case "THROW_B":
 			createKeys("4 _ B");
 			break;
-		case	"STAND_A"	:
+		case "STAND_A":
 			createKeys("A");
 			break;
-		case	"STAND_B"	:
+		case "STAND_B":
 			createKeys("B");
 			break;
-		case	"CROUCH_A"	:
+		case "CROUCH_A":
 			createKeys("2 _ A");
 			break;
-		case	"CROUCH_B"	:
+		case "CROUCH_B":
 			createKeys("2 _ B");
 			break;
-		case	"AIR_A"	:
+		case "AIR_A":
 			createKeys("A");
 			break;
-		case	"AIR_B"	:
+		case "AIR_B":
 			createKeys("B");
 			break;
-		case	"AIR_DA"	:
+		case "AIR_DA":
 			createKeys("2 _ A");
 			break;
-		case	"AIR_DB"	:
+		case "AIR_DB":
 			createKeys("2 _ B");
 			break;
-		case	"STAND_FA"	:
+		case "STAND_FA":
 			createKeys("6 _ A");
 			break;
-		case	"STAND_FB"	:
+		case "STAND_FB":
 			createKeys("6 _ B");
 			break;
-		case	"CROUCH_FA"	:
+		case "CROUCH_FA":
 			createKeys("3 _ A");
 			break;
-		case	"CROUCH_FB"	:
+		case "CROUCH_FB":
 			createKeys("3 _ B");
 			break;
-		case	"AIR_FA"	:
+		case "AIR_FA":
 			createKeys("9 _ A");
 			break;
-		case	"AIR_FB"	:
+		case "AIR_FB":
 			createKeys("9 _ B");
 			break;
-		case	"AIR_UA"	:
+		case "AIR_UA":
 			createKeys("8 _ A");
 			break;
-		case	"AIR_UB"	:
+		case "AIR_UB":
 			createKeys("8 _ B");
 			break;
-		case	"STAND_D_DF_FA"	:
+		case "STAND_D_DF_FA":
 			createKeys("2 3 6 _ A");
 			break;
-		case	"STAND_D_DF_FB"	:
+		case "STAND_D_DF_FB":
 			createKeys("2 3 6 _ B");
 			break;
-		case	"STAND_F_D_DFA"	:
+		case "STAND_F_D_DFA":
 			createKeys("6 2 3 _ A");
 			break;
-		case	"STAND_F_D_DFB"	:
+		case "STAND_F_D_DFB":
 			createKeys("6 2 3 _ B");
 			break;
-		case	"STAND_D_DB_BA"	:
+		case "STAND_D_DB_BA":
 			createKeys("2 1 4 _ A");
 			break;
-		case	"STAND_D_DB_BB"	:
+		case "STAND_D_DB_BB":
 			createKeys("2 1 4 _ B");
 			break;
-		case	"AIR_D_DF_FA"	:
+		case "AIR_D_DF_FA":
 			createKeys("2 3 6 _ A");
 			break;
-		case	"AIR_D_DF_FB"	:
+		case "AIR_D_DF_FB":
 			createKeys("2 3 6 _ B");
 			break;
-		case	"AIR_F_D_DFA"	:
+		case "AIR_F_D_DFA":
 			createKeys("6 2 3 _ A");
 			break;
-		case	"AIR_F_D_DFB"	:
+		case "AIR_F_D_DFB":
 			createKeys("6 2 3 _ B");
 			break;
-		case	"AIR_D_DB_BA"	:
+		case "AIR_D_DB_BA":
 			createKeys("2 1 4 _ A");
 			break;
-		case	"AIR_D_DB_BB"	:
+		case "AIR_D_DB_BB":
 			createKeys("2 1 4 _ B");
 			break;
-		case	"STAND_D_DF_FC"	:
+		case "STAND_D_DF_FC":
 			createKeys("2 3 6 _ C");
 			break;
 		default:
@@ -169,109 +188,137 @@ public class CommandCenter {
 
 	}
 
-	/*
-	 * Keyの作成
+	/**
+	 * コマンド名に対応したキーを作成する．
+	 *
+	 * @param str
+	 *            コマンド名
 	 */
-	private void createKeys(String str){
+	private void createKeys(String str) {
 		Key buf;
 		String[] commands = str.split(" ");
-		if(!this.frameData.getCharacter(playerNumber).isFront()){
+		if (!this.frameData.getCharacter(playerNumber).isFront()) {
 			commands = reverseKey(commands);
 		}
-		
+
 		int index = 0;
-		while(index < commands.length){
+		while (index < commands.length) {
 			buf = new Key();
-			if(commands[index].equals("L") || commands[index].equals("4")){
-				buf.L=true;
-			}else if(commands[index].equals("R") || commands[index].equals("6")){
-				buf.R=true;
-			}else if(commands[index].equals("D") || commands[index].equals("2")){
-				buf.D=true;
-			}else if(commands[index].equals("U") || commands[index].equals("8")){
-				buf.U=true;
-			}else if(commands[index].equals("LD") || commands[index].equals("1")){
-				buf.L=true;
-				buf.D=true;
-			}else if(commands[index].equals("LU") || commands[index].equals("7")){
-				buf.L=true;
-				buf.U=true;
-			}else if(commands[index].equals("RD") || commands[index].equals("3")){
-				buf.R=true;
-				buf.D=true;
-			}else if(commands[index].equals("RU") || commands[index].equals("9")){
-				buf.R=true;
-				buf.U=true;
+			if (commands[index].equals("L") || commands[index].equals("4")) {
+				buf.L = true;
+			} else if (commands[index].equals("R") || commands[index].equals("6")) {
+				buf.R = true;
+			} else if (commands[index].equals("D") || commands[index].equals("2")) {
+				buf.D = true;
+			} else if (commands[index].equals("U") || commands[index].equals("8")) {
+				buf.U = true;
+			} else if (commands[index].equals("LD") || commands[index].equals("1")) {
+				buf.L = true;
+				buf.D = true;
+			} else if (commands[index].equals("LU") || commands[index].equals("7")) {
+				buf.L = true;
+				buf.U = true;
+			} else if (commands[index].equals("RD") || commands[index].equals("3")) {
+				buf.R = true;
+				buf.D = true;
+			} else if (commands[index].equals("RU") || commands[index].equals("9")) {
+				buf.R = true;
+				buf.U = true;
 			}
 			// 2 4 1 _ Bなどの"_"直後の部分を直前のKeyに含めるための処理
-			//"_"直後は"A","B","C"のみ対応
-			if(index+2<commands.length && commands[index+1].equals("_")){
-				index +=2;
+			// "_"直後は"A","B","C"のみ対応
+			if (index + 2 < commands.length && commands[index + 1].equals("_")) {
+				index += 2;
 			}
-			if(commands[index].equals("A")){
-				buf.A=true;
-			}else if(commands[index].equals("B")){
-				buf.B=true;
-			}else if(commands[index].equals("C")){
-				buf.C=true;
+			if (commands[index].equals("A")) {
+				buf.A = true;
+			} else if (commands[index].equals("B")) {
+				buf.B = true;
+			} else if (commands[index].equals("C")) {
+				buf.C = true;
 			}
 			skillKey.add(buf);
 			index++;
 		}
 	}
 
-	public void setFrameData(FrameData frameData, boolean playerNumber){
+	/**
+	 * 現在のフレームデータとP1,P2を表すboolean変数をセットする．
+	 *
+	 * @param frameData
+	 *            現在のフレームデータ
+	 * @param playerNumber
+	 *            The character's side flag.<br>
+	 *            {@code true} if the character is P1, or {@code false} if P2.
+	 */
+	public void setFrameData(FrameData frameData, boolean playerNumber) {
 		this.frameData = frameData;
 		this.playerNumber = playerNumber;
 	}
-	
+
 	/**
-	 * @return　実行待ちのコマンドが存在しているかどうか
+	 * リストにまだ処理されてないキーがあるかどうかを返す．
+	 *
+	 * @return {@code true} リストにまだ処理されてないキーがある，{@code false} otherwise.
 	 */
-	public boolean getSkillFlag(){
+	public boolean getSkillFlag() {
 		return !this.skillKey.isEmpty();
 	}
 
 	/**
-	 * 実行待ちのコマンドリストから先頭の要素を渡す
-	 * 渡した先頭要素はCommandCenter上から削除される
-	 * @return
+	 * まだ処理されてないキーを格納しているリストから先頭の要素を渡す．<br>
+	 * 渡した先頭要素はCommandCenter上から削除される．
+	 *
+	 * @return 次に処理するキー
 	 */
-	public Key getSkillKey(){
-		if(!this.skillKey.isEmpty()){
+	public Key getSkillKey() {
+		if (!this.skillKey.isEmpty()) {
 			return this.skillKey.pollFirst();
-		}else{
+		} else {
 			return new Key();
 		}
 	}
-	
-	//実行待ちのコマンドリストを全て返す
-	public Deque<Key> getSkillKeys(){
+
+	/**
+	 * まだ処理されてないキーを格納しているリストを返す．
+	 *
+	 * @return まだ処理されてないキーを格納しているリスト
+	 */
+	public Deque<Key> getSkillKeys() {
 		return new LinkedList<Key>(this.skillKey);
 	}
-	
-	//実行待ちのコマンドをキャンセルする
-	public void skillCancel(){
+
+	/**
+	 * まだ処理されてないキーを全て削除する．
+	 */
+	public void skillCancel() {
 		this.skillKey.clear();
 	}
-	
-	//KeyDataの反転
-	private String[] reverseKey(String[] commands){
+
+	/**
+	 * キャラクターの向いている方向に従って，コマンドの左右反転処理を行う．
+	 *
+	 * @param commands
+	 *            アクション名から変換されたコマンドを格納している配列
+	 *
+	 * @return 左右反転処理を行ったコマンドを格納している配列
+	 */
+	private String[] reverseKey(String[] commands) {
 		String[] buffer = new String[commands.length];
-		for(int i = 0; i < commands.length; i++){
-			if(commands[i].equals("L") || commands[i].equals("4")){
+		for (int i = 0; i < commands.length; i++) {
+			if (commands[i].equals("L") || commands[i].equals("4")) {
 				buffer[i] = "6";
-			}else if(commands[i].equals("R") || commands[i].equals("6")){
+			} else if (commands[i].equals("R") || commands[i].equals("6")) {
 				buffer[i] = "4";
-			}else if(commands[i].equals("LD") || commands[i].equals("1")){
+			} else if (commands[i].equals("LD") || commands[i].equals("1")) {
 				buffer[i] = "3";
-			}else if(commands[i].equals("LU") || commands[i].equals("7")){
+			} else if (commands[i].equals("LU") || commands[i].equals("7")) {
 				buffer[i] = "9";
-			}else if(commands[i].equals("RD") || commands[i].equals("3")){
+			} else if (commands[i].equals("RD") || commands[i].equals("3")) {
 				buffer[i] = "1";
-			}else if(commands[i].equals("RU") || commands[i].equals("9")){
+			} else if (commands[i].equals("RU") || commands[i].equals("9")) {
 				buffer[i] = "7";
-			}else{
+			} else {
 				buffer[i] = commands[i];
 			}
 		}
--- src/aiinterface/ThreadController.java
+++ src/aiinterface/ThreadController.java
@@ -1,33 +1,83 @@
 package aiinterface;
 
+/**
+ * AIの実行のタイミングなどのスレッド関連の処理を扱うクラス．
+ */
 public class ThreadController {
+
+	/**
+	 * ThreadController唯一のインスタンス
+	 */
 	private static ThreadController threadController = new ThreadController();
 
-	private Object AI1, AI2;
+	/**
+	 * P1のAIの処理の開始のタイミングを管理するオブジェクト．
+	 */
+	private Object AI1;
 
-	private boolean processedAI1, processedAI2;
+	/**
+	 * P2のAIの処理の開始のタイミングを管理するオブジェクト．
+	 */
+	private Object AI2;
+
+	/**
+	 * P1のAIの処理が終わったかどうかを表すフラグ．<br>
+	 * Fastmodeのときのみ使用される．
+	 */
+	private boolean processedAI1;
+
+	/**
+	 * P2のAIの処理が終わったかどうかを表すフラグ．<br>
+	 * Fastmodeのときのみ使用される．
+	 */
+	private boolean processedAI2;
+
+	/**
+	 * 各AIの処理を同時に始めるための同期用オブジェクト
+	 */
 	private Object endFrame;
 
+	/**
+	 * フィールド変数を初期化するクラスコンストラクタ
+	 */
 	private ThreadController() {
 		this.AI1 = new Object();
 		this.AI2 = new Object();
 		this.endFrame = new Object();
+
 		resetProcessedFlag();
 	}
 
+	/**
+	 * ThreadControllerクラスの唯一のインスタンスを取得する．
+	 *
+	 * @return ThreadControllerクラスの唯一のインスタンス
+	 */
 	public static ThreadController getInstance() {
 		return threadController;
 	}
 
+	/**
+	 * 各AIの処理を再開させる．
+	 */
 	public void resetAllAIsObj() {
-		synchronized (AI1) {
+		synchronized (this.AI1) {
 			this.AI1.notifyAll();
 		}
-		synchronized (AI2) {
+		synchronized (this.AI2) {
 			this.AI2.notifyAll();
 		}
 	}
 
+	/**
+	 * 引数に指定したキャラクターの同期用オブジェクトを返す．
+	 *
+	 * @param playerNumber
+	 *            The character's side flag.<br>
+	 *            {@code true} if the character is P1, or {@code false} if P2.
+	 *
+	 * @return 引数に指定したキャラクターの同期用オブジェクト
+	 */
 	public Object getAIsObject(boolean playerNumber) {
 		if (playerNumber)
 			return this.AI1;
@@ -35,31 +85,54 @@ public class ThreadController {
 			return this.AI2;
 	}
 
+	/**
+	 * 1フレーム分のゲームの処理が終わったことを示すオブジェクトを返す．
+	 *
+	 * @return 1フレーム分のゲームの処理が終わったことを示すオブジェクト．
+	 */
 	public Object getEndFrame() {
 		return this.endFrame;
 	}
 
+	/**
+	 * 各AIの処理が終わったかどうかを表すフラグを{@code false}にする．<br>
+	 * Fastmodeのときのみ使用される．
+	 */
 	private void resetProcessedFlag() {
 		this.processedAI1 = false;
 		this.processedAI2 = false;
 	}
 
-	synchronized public void notifyEndProcess(boolean playerNumber){
+	/**
+	 * 引数に指定したキャラクターの1フレーム分の処理が終わったことをセットする．<br>
+	 * セット後に，両方のAIが処理を終えているかどうかをチェックする．<br>
+	 * Fastmodeのときのみ使用される．
+	 *
+	 * @param playerNumber
+	 *            The character's side flag.<br>
+	 *            {@code true} if the character is P1, or {@code false} if P2.
+	 */
+	synchronized public void notifyEndProcess(boolean playerNumber) {
 		if (playerNumber) {
-			processedAI1 = true;
+			this.processedAI1 = true;
 		} else {
-			processedAI2 = true;
+			this.processedAI2 = true;
 		}
 		this.checkEndFrame();
 	}
 
-	public void checkEndFrame() {
-		if (processedAI1 && processedAI2) {
+	/**
+	 * 現在のフレームにおいて，両AIが処理を終えているかどうかをチェックする．<br>
+	 * 終えている場合は，次のフレームの処理を開始させる．<br>
+	 * Fastmodeのときのみ使用される．
+	 */
+	private void checkEndFrame() {
+		if (this.processedAI1 && this.processedAI2) {
 			synchronized (this.endFrame) {
 				this.endFrame.notifyAll();
 			}
-			processedAI1 = false;
-			processedAI2 = false;
+			this.processedAI1 = false;
+			this.processedAI2 = false;
 		}
 	}
 
