30dd32c8d3dc77d7d198df021bd758b53bfb31b0
Staciiaz <chollakorn.nimp@gmail.com>
2023-10-22 23:45:40 +0900

    modified run script for Unix-based OS

--- run-linux-amd64.sh
+++ run-linux-amd64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/linux/amd64/*:./lib/grpc/* Main --limithp 400 400 --grey-bg --grpc
+java -cp FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/linux/amd64/*:./lib/grpc/* Main --limithp 400 400 --grey-bg
--- run-macos-arm64.sh
+++ run-macos-arm64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -XstartOnFirstThread -cp FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/macos/arm64/*:./lib/grpc/* Main --limithp 400 400 --grey-bg --grpc
+java -XstartOnFirstThread -cp FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/macos/arm64/*:./lib/grpc/* Main --limithp 400 400 --grey-bg
