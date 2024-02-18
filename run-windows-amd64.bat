074cdaa202274aca3979bb76100af05cb72f8024
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-02-19 02:10:16 +0900

    Update run-windows-amd64.bat

--- run-windows-amd64.bat
+++ run-windows-amd64.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -cp FightingICE.jar;./lib/*;./lib/lwjgl/*;./lib/lwjgl/natives/windows/amd64/*;./lib/grpc/*; Main --limithp 400 400 --grpc-auto --non-delay 0 --grey-bg
+java -cp FightingICE.jar;./lib/*;./lib/lwjgl/*;./lib/lwjgl/natives/windows/amd64/*;./lib/grpc/*; Main --limithp 400 400 --grey-bg
